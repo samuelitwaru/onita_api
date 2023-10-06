e992ffb49a0fdbeda9c4e3ae33a67bb9b5c16dd1
samuelitwaru <samuelitwaru@gmail.com>
2023-10-06 08:31:24 +0300

    added allowed hosts *

--- onita_api/settings.py
+++ onita_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0o!kzyoe!a=^1cqnf15c$y*z&=2zabt%9&!-@-nd6ztg!^_0o_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
