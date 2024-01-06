3af4004706a704cd9108fd4dd5b7883ab8600545
samuelitwaru <samuelitwaru@gmail.com>
2024-01-06 21:43:42 +0300

    added base to templates dir

--- onita_api/settings.py
+++ onita_api/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'onita_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
