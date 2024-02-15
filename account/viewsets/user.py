16f5ca03bc7e1d538761acd5ade9b2cc9624023c
samuelitwaru <samuelitwaru@gmail.com>
2024-02-15 09:16:26 +0300

    more;

--- account/viewsets/user.py
+++ account/viewsets/user.py
@@ -6,7 +6,8 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate, login, logout
 from account.filters import BaseFilter
-from account.serializers import UserSerializer
+from account.serializers import LoginSerializer, SchoolSerializer, StudentSerializer, TeacherSerializer, UserSerializer
+from api.models import School, Student, Teacher
 
 
 
@@ -20,6 +21,24 @@ class UserViewSet(viewsets.ModelViewSet):
         f = BaseFilter(self.queryset, params)
         queryset = f.filter()
         return queryset
+    
+    @action(detail=False, methods=['POST'], name='login', url_path=r'login', serializer_class=LoginSerializer)
+    def login(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            # user_data = UserSerializer(user).data
+            # user_data['groups'] = [group.name for group in user.groups.all()]
+            student = StudentSerializer(Student.objects.filter(user=user).first()).data
+            teacher = TeacherSerializer(Teacher.objects.filter(user=user).first()).data
+            school = SchoolSerializer(School.objects.filter(user=user).first()).data
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'student':student, 'teacher':teacher, 'school':school})
+        else:
+            return Response({'error': ['Invalid credentials']}, status=status.HTTP_401_UNAUTHORIZED)
     
     @action(detail=False, methods=['GET'], name='logout', url_path=r'logout')
     def logout(self, request, *args, **kwargs):
